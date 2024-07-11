378ac933e361bff20a7474cfd2f9512a03c4a14d
Hassaan-Ahmedd <hassutechap@gmail.com>
2024-07-11 23:24:05 +0500

    Challenge Mobile Nav completed 100%/100% done !

---
+++ challenge-3/Mobile-Navigataion/script.js
@@ -0,0 +1,11 @@
+function navClick(navName){
+
+    let nav1 = document.getElementById('nav1');
+    let nav2 = document.getElementById('nav2');
+    let nav3 = document.getElementById('nav3');
+    let nav4 = document.getElementById('nav4');
+
+    // alert("HHHHHH")
+    
+    nav1.setAttribute('stroke','#000000');
+}
